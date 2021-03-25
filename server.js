4b61d7de68a6aba5661163ba1e5668bacf464d42
unknown <felipe_pansani@outlook.com>
2021-03-25 20:17:35 -0300

    8th try

--- server.js
+++ server.js
@@ -9,6 +9,6 @@ app.get(/.*/, (req, res) => {
     res.sendFile(path.join(__dirname, '/dist/index.html'))
 })
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT;
 app.listen(port);
 console.log(`app is listening on port: ${port}`)
